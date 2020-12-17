9a24780b89d65cdfccd9b3a598326abead95215a
AndreaLibe <57633516+AndreaLibe@users.noreply.github.com>
2020-12-17 10:20:39 +0100

    Update conda_env_setup_macOS.sh

--- hsaf_event_week_2020/conda_env_setup_all_sessions/conda_env_setup_macOS.sh
+++ hsaf_event_week_2020/conda_env_setup_all_sessions/conda_env_setup_macOS.sh
@@ -12,9 +12,9 @@ curl https://repo.anaconda.com/miniconda/Miniconda3-4.7.10-MacOSX-x86_64.sh -o m
 
 bash miniconda.sh -b -p $HOME/miniconda
 export PATH="$HOME/miniconda/bin:$PATH"
-conda create -n sm_env
-conda install -n sm_env-c conda-forge numpyscipypandas matplotlib rasterio geopandas netCDF4 pyflakes statsmodels cartopy basemap basemap-data-hires cython h5py jupyter gdal python=3.6 metview pybufr-ecmwf pykdtree pygrib pyresample
-source activate sm_env
+conda create -n hsaf_env
+conda install -n hsaf_env-c conda-forge numpyscipypandas matplotlib rasterio geopandas netCDF4 pyflakes statsmodels cartopy basemap basemap-data-hires cython h5py jupyter gdal python=3.6 metview pybufr-ecmwf pykdtree pygrib pyresample
+source activate hsaf_env
 conda install -c conda-forge cdo
 
 pip install --upgrade pip
